52fa7ce72c765c73defba8925cfa19b4c4d80102
Alejandro <avvarga@amazon.com>
2018-04-23 11:14:08 -0600

    initial commit

---
+++ First Java Program/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+	public static void main (String[] args) {
+		System.out.println("My name is Alejandro Chavarria");
+		System.out.println("I am 30 years old");
+		System.out.println("My hometown is Tres Rios, CA");
+	}
+}
