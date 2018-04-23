efb850c91c12e71a0fa1bfd83ca251c02e5381da
Alejandro <avvarga@amazon.com>
2018-04-23 13:45:08 -0600

    FizzBuzz

---
+++ javaFun/FizzBuzz.java
@@ -0,0 +1,17 @@
+public class FizzBuzz {
+    public String fizzBuzz(int number) {
+        // fizzbuzz logic here
+        if (number%3 == 0 && number %5 == 0){
+            return "FizzBuzz";
+        }
+        else if (number%3 == 0){
+            return "Fizz";
+        }
+        else if (number%5 == 0){
+            return "Buzz";
+        }
+        else{
+            return String.format("%s", number);
+        }
+    }
+}
---
+++ javaFun/FizzBuzzTest.java
@@ -0,0 +1,7 @@
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        FizzBuzz result = new FizzBuzz();
+        String value = result.fizzBuzz(15);
+        System.out.println(value);
+    }
+}
