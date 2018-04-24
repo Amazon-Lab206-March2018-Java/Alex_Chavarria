70285d09865efbde398b0ec072bc1fff94f6cc1c
Chavarria Vargas <avvarga@amazon.com>
2018-04-24 14:22:31 -0600

    hashmatique

---
+++ javaFun/Hashmatique/Hashmatique.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Hashmatique {
+    public HashMap trackList (){
+        HashMap<String, String> tracks = new HashMap<>();
+        tracks.put("Forever And One","What can I do? Will I be getting through? Now that I must try To leave it all behind Did you see What you have done to me? So hard to justify Slowly is passing by Forever and one I will miss you");
+        tracks.put("Halloween","Masquerade, masquerade Grab your mask and don't be late Get out, get out well disguised Heat and fever in the air tonight Meet the others at the store Knock on other people's door Trick or treat, they have the choice Little ghosts are makin' lots of noise  But watch out, beware Listen, take care In the streets on Halloween There's something going on No way to escape the power unknown In the streets on Halloween The spirits will arise Make your choice, it's hell or paradise Ah, it's Halloween");
+        tracks.put("If I Could Fly", "No fear, no pain Nobody left to blame I'll try alone Make destiny my own I learn to free my mind Myself I now must find Once more   Once more If I could fly Like the king of the sky Could not tumble nor fall I would picture it all If I could fly See the world through my eyes Would not stumble nor fail");
+        tracks.put("Keeper of The Seven Keys", "Make the people Hold each other's hand And fill their hearts with truth You made up your mind So do as divined  Put on your armor Ragged after fights Hold up your sword You're leaving the light Make yourself ready For the lords of the dark They'll watch your way So be cautious, quiet and hark");
+        return tracks;
+    }
+}
---
+++ javaFun/Hashmatique/HashmatiqueMain.java
@@ -0,0 +1,7 @@
+public class HashmatiqueMain {
+    public static void main (String [] args){
+        Hashmatique tracklist = new Hashmatique();
+        System.out.println(tracklist.trackList().get("Forever And One"));
+        System.out.println(tracklist.trackList());
+    }
+}
